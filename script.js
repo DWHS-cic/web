ebdb29805c0b61f8adeb2e655085f7293b3b64fe
ruei._.97 <9794tingian@gmail.com>
2025-08-19 23:25:09 +0800

    首頁改動-首頁下方開始

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ if (hamburger && navMenu) {
     navMenu.classList.toggle('active');
   });
 }
+
+
+const track = document.getElementById('track-multi');
+let speed = 1; // 每次移動 px
+let scrollPos = 0;
+
+function animate() {
+  scrollPos += speed;
+  if(scrollPos >= track.scrollWidth / 2) {
+    scrollPos = 0;
+  }
+  track.style.transform = `translateX(-${scrollPos}px)`;
+  requestAnimationFrame(animate);
+}
+
+// 複製一次內容，用於無縫循環
+track.innerHTML += track.innerHTML;
+animate();
